e9f6e2006879b879e748a823b9278544b3326a5c
IONHAX04 <thirukumara04@gmail.com>
2025-01-03 18:15:17 +0530

    Home page drafted

--- src/components/01-Home/Home.tsx
+++ src/components/01-Home/Home.tsx
@@ -34,13 +34,13 @@ const Home: React.FC = () => {
         <div className="homePageIntroCont">
           <section className="w-full px-2 py-12 grid grid-cols-1 md:grid-cols-2 items-center gap-8 max-w-7xl mx-auto">
             <div>
-              <span className="block mb-4 text-xl font-bold text-[#ffff] font-medium">
+              <span className="block mb-4 lg:text-xl mt-10 text-xl font-bold text-[#ffff] font-medium">
                 Courier Services in India
               </span>
-              <h3 className="text-5xl text-[#fcb026] font-semibold">
+              <h3 className="lg:text-5xl text-3xl text-[#fcb026] font-semibold">
                 Fast, Reliable & Affordable Delivery Solutions{" "}
               </h3>
-              <p className="text-xl font-medium text-[#ffff] my-4 md:my-6">
+              <p className="lg:text-xl font-medium text-[#ffff] my-4 md:my-6">
                 A leading courier service provider in India offering fast,{" "}
                 <br /> secure, and affordable delivery solutions
               </p>
